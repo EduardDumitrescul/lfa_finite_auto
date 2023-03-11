b0fcf051d9578fc0f2729ed50fa8e962e5897a72
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-03-11 09:49:00 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include "DFA.h"
+#include "NFA.h"
+
+void testDFA() {
+    DFA dfa;
+    dfa.readFromFile("../testfiles/dfa.in");
+    std::ifstream fin("../testfiles/words.txt");
+    int words;
+    fin >> words;
+    while(words--) {
+        std::string word;
+        fin >> word;
+        std::cout << word << ' ' << dfa.checkWord(word) << '\n';
+    }
+    fin.close();
+}
+
+void testNFA() {
+    NFA nfa;
+    nfa.readFromFile("../testfiles/nfa.in");
+    std::ifstream fin("../testfiles/words.txt");
+    int words;
+    fin >> words;
+    while(words--) {
+        std::string word;
+        fin >> word;
+        std::cout << word << ' ' << nfa.checkWord(word) << '\n';
+    }
+    fin.close();
+}
 
 int main() {
-    DFA dfa;
-    dfa.runFromFile("../testfiles/dfa.in", "../testfiles/dfa.out");
-
+    testDFA();
+    testNFA();
 
     return 0;
 }
